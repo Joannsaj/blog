b22d34ede4fcfb55a5d7b3a7493ed36b5b861cb8
joansaj <joannsaj@gmail.com>
2020-10-31 16:05:20 +0300

    added user test

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    blogs = db.relationship('Blog',backref = 'role',lazy="dynamic")
+   
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
 class Quote:
     def __init__(self, author, id, quote, permalink):
         self.author = author
@@ -8,3 +33,4 @@ class Quote:
 class Blog:
     def __init__(self, text):
         self.text = text     
+        user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
