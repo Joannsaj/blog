ac68cf47be8ce881a448d870d6a435bda4027f6e
joansaj <joannsaj@gmail.com>
2020-10-31 16:49:55 +0300

    added login

--- app/request.py
+++ app/request.py
@@ -27,33 +27,3 @@ def get_quotes():
 
     return quote_object
 
-# def get_quotes():
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     with urllib.request.urlopen(base_url) as url:
-#         get_quotes_data = url.read()
-#         get_quotes_response = json.loads(get_quotes_data)
-
-#         quote_results = None
-
-#         if get_quotes_response:
-#             quote_results_list = get_quotes_response
-#             quote_results = process_results(quote_results_list)
-
-
-#     return quote_results
-
-# def process_results(quote_list):
-   
-#     quote_results = []
-#     for quote_item in quote_list:
-#         author = quote_item.get('author')
-#         id = quote_item.get('id')
-#         quote = quote_item.get('quote')
-#         permalink = quote_item.get('permalink')
-
-#         quote_object = Quote(author, id, quote, permalink)
-#         quote_results.append(quote_object)
-
-#     return quote_results    
